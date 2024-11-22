9a34fe2df632458eb16bda1c1efc660ea49e5891
YDRR123 <125160236+Yeison123456@users.noreply.github.com>
2024-11-21 20:48:11 -0500

    Added background particles to index.astro file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,ts,tsx}", // Incluye todos los archivos relevantes
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
